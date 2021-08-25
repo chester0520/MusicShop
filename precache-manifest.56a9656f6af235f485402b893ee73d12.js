cdc3b0fece8ebfc9e5d59b73641cbb070acd649e
chester0520 <80014532+chester0520@users.noreply.github.com>
2021-08-25 08:16:55 +0000

    Deploying to gh-pages from @ chester0520/MusicShop@6527d6983752d84b091cf61afb8e5a07404d5f71 🚀

---
+++ precache-manifest.56a9656f6af235f485402b893ee73d12.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff52c835341c62dfa5f",
+    "url": "css/app.efd48949.css"
+  },
+  {
+    "revision": "0299193144a0c2a41e3d",
+    "url": "css/chunk-vendors.ca0d4016.css"
+  },
+  {
+    "revision": "04b7fd97f88b82dccce5ec446ccc29e6",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.04b7fd97.woff"
+  },
+  {
+    "revision": "da2721c68b4bc80db8d4c404f76b118c",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.da2721c6.woff"
+  },
+  {
+    "revision": "bf0f407102faf3a0b521d3b545f547a5",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.bf0f4071.woff"
+  },
+  {
+    "revision": "68d6dabfe54e245e7d5d5c16c3c4b1a9",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.68d6dabf.woff"
+  },
+  {
+    "revision": "64bba9c4e8156c152050c657e9d24bf1",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.64bba9c4.woff"
+  },
+  {
+    "revision": "dc3e086fc0c5addc09702e111d2adb42",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.dc3e086f.woff"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "fonts/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "fonts/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "fonts/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "fonts/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "fonts/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "fonts/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "77b1b72456c0ebd4b4cb0c862b6a8263",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.77b1b724.woff"
+  },
+  {
+    "revision": "519b8674b0b5dc53b39f9b06d2d77019",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.519b8674.woff2"
+  },
+  {
+    "revision": "a5b5d4166c094c433226b2d93077cab6",
+    "url": "img/iconfont.a5b5d416.svg"
+  },
+  {
+    "revision": "88590f227f7866825eb86af51fb54c67",
+    "url": "index.html"
+  },
+  {
+    "revision": "428a8f02600f2cad129e",
+    "url": "js/AdminNews.79905d56.js"
+  },
+  {
+    "revision": "5843fddad836b92a3f76",
+    "url": "js/AdminNews~AdminOrders~AdminProducts~cart~login~personal~product~register~service.19de3b03.js"
+  },
+  {
+    "revision": "0672a76a424e158fbc4d",
+    "url": "js/AdminNews~AdminOrders~AdminProducts~cart~order.509bcc65.js"
+  },
+  {
+    "revision": "18aa41e2cc507b754761",
+    "url": "js/AdminOrders.4fb84061.js"
+  },
+  {
+    "revision": "e2ef11e723206a1e7d70",
+    "url": "js/AdminProducts.a2636f3e.js"
+  },
+  {
+    "revision": "140eb23394d17525a945",
+    "url": "js/aboutus.5cf9411f.js"
+  },
+  {
+    "revision": "154416207d62faccc37d",
+    "url": "js/admin.6b83d629.js"
+  },
+  {
+    "revision": "0ff52c835341c62dfa5f",
+    "url": "js/app.75043c45.js"
+  },
+  {
+    "revision": "cf1dc7d3903ea621fe3c",
+    "url": "js/cart.79f8831f.js"
+  },
+  {
+    "revision": "0299193144a0c2a41e3d",
+    "url": "js/chunk-vendors.c47c17ae.js"
+  },
+  {
+    "revision": "f0ee94ea1571b03e68f2",
+    "url": "js/login.0027dfdf.js"
+  },
+  {
+    "revision": "11ae5b5044121ddfb4fb",
+    "url": "js/news.1d18d5af.js"
+  },
+  {
+    "revision": "0ea1a49fb13a9fe1aab4",
+    "url": "js/order.876b24c5.js"
+  },
+  {
+    "revision": "591aacda22cb25042865",
+    "url": "js/personal.429f75f4.js"
+  },
+  {
+    "revision": "aad6801ef93969b0dff8",
+    "url": "js/product.4c728f50.js"
+  },
+  {
+    "revision": "176021606b159c39468f",
+    "url": "js/productlist.9c69ec4e.js"
+  },
+  {
+    "revision": "00305adfb8a05737a9d9",
+    "url": "js/register.1752a3be.js"
+  },
+  {
+    "revision": "4c094e16b03a19a19811",
+    "url": "js/service.dd2eb15d.js"
+  },
+  {
+    "revision": "f1c4ba9164554f3c48d8",
+    "url": "js/userinfo.1c4273bc.js"
+  },
+  {
+    "revision": "1ecee0c343b1bc0443f2e6eba14ed6ff",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
